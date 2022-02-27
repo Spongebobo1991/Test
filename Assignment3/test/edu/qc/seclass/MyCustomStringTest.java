1b9f5b0344d709acb36d3b805c476bbd8e4c3f21
Spongebobo1991 <binggao91@gmail.com>
2022-02-27 10:00:29 -0500

    11

--- Assignment3/test/edu/qc/seclass/MyCustomStringTest.java
+++ Assignment3/test/edu/qc/seclass/MyCustomStringTest.java
@@ -107,7 +107,7 @@ public class MyCustomStringTest {
         assertEquals("aol3 rn ohdow!", mycustomstring.getEveryNthCharacterFromBeginningOrEnd(3, true));
     }
 
-    //test to print out the entire string by itteration
+    //test to print out the entire string by itteration1
     @Test
     public void testGetEveryNthCharacterFromBeginningOrEnd9() {
         mycustomstring.setString("Print me out completley");
